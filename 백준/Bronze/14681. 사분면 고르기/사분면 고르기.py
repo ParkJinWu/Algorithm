5a7b5eeb757a7d0ef5024f649ec0aace5b77e4b3
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-09-01 23:42:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0 : #Quadrant 1
+    print(1)
+elif x < 0 and y > 0 : #Quadrant 2
+    print(2)
+elif x < 0 and y < 0 : #Quadrant 3
+    print(3)
+else:                  #Quadrant 4
+    print(4)
