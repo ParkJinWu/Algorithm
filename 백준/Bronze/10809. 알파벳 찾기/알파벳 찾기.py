98a2af25bf809aeca8cee18fde1e58b48b29a542
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-09-04 23:41:24 +0900

    Update 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -3,4 +3,4 @@ word = input()
 alphabet = list(range(97,123))  # 아스키코드 숫자 범위
 
 for x in alphabet :
-    print(word.find(chr(x)), end = ' ' )
+    print(word.find(chr(x)), end = ' ' )#chr()함수를 이용해 아스키 코드 값을 입력받아 그 코드에 해당하는 문자 출력
