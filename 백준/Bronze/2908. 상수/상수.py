2b14037d2559b922c08f79b91fbd246641a23723
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-09-06 01:30:24 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,11 +1,11 @@
 # BOJ - 2908 상수
-A,B = map(list,input().split())
+A,B = map(list,input().split()) #map함수를 이용해 입력받은 요소를 list로 변환해준다.
 
-A.reverse()
+A.reverse() #reverse()함수를 이용해 뒤집는다
 B.reverse()
 
 A1 = ''.join(A)
 B1 = ''.join(B)
 
-if A1 > B1: print(A1)
-else:print(B1)
+
+print(max(A1,B1))
