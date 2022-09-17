c89efbdd14667ef3e1b8c73a35007ef4b924deed
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-09-17 19:26:20 +0900

    [Bronze I] Title: 소인수분해, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,15 +1,11 @@
-# BOJ - 11653 소인수분해
-# 1보다 큰 자연수를 소인수(소수인 인수)들만의 곱
-
-n = int(input())
-
-if n == 1:
-    print("")
+N = int(input())
 
 
-while n!=1:
-    for i in range(2, n+1):
-        if n % i == 0:
-            print(i)
-            n = n // i
-            break
+
+for i in range(2,int(N ** 0.5)+1):
+    while N % i == 0:
+        print(i)
+        N //= i
+
+if N > 1:
+    print(N)
