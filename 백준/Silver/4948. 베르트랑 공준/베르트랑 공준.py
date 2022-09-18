063dfeddad0bb7c317dddb411df79949df448652
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-09-18 14:55:36 +0900

    [Silver III] Title: 베르트랑 공준, Time: 1464 ms, Memory: 42052 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,33 @@
+#BOJ - 4948 베르트랑 공준
+from math import sqrt
+
+def check_prime(num):
+    if num == 1:
+        return False
+
+    for x in range(2,int(sqrt(num)+1)):
+        if num % x == 0:
+            return False
+
+    return True
+
+li = list(range(2,246912))
+prime_list = []
+
+
+for i in li:
+    if check_prime(i):
+        prime_list.append(i)
+
+
+while True:
+    prime_count = 0
+    n = int(input())
+
+    if n == 0 :
+        break
+
+    for i in prime_list:
+        if n < i <= n*2:
+            prime_count+=1
+    print(prime_count)
