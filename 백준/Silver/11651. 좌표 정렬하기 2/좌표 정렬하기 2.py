99565ab157216a58cb5688262cb3dca01e0a7e7d
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-10-15 22:12:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 4436 ms, Memory: 43908 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+#BOJ - 11651 좌표 정렬하기2
+
+n = int(input())
+
+li = []
+for i in range(n):
+    x,y = map(int,input().split())
+    li.append((y,x))#y가 기준이기 때문에 y를 먼저 넣기위해 y가 x보다 앞에와야된다.
+
+li.sort()
+
+for i in range(n):
+    print(li[i][1],li[i][0])
