ec3b0b915ab5700e43fe07a92e8163a6d3d8ff11
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-10-19 16:24:48 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,4 @@
 def solution(n):
-    answer = []
-    for i in range(1,n+1):
-        if n % i == 0:
-            answer.append(i)    
+    answer = [x for x in range(1,n+1) if n%x==0]
+    
     return answer
