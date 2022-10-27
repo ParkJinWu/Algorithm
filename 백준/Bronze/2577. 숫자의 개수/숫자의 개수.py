bd9cf8617c707e0a8a7ed5aec97c08ce0d815998
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-10-27 18:09:16 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -2,9 +2,7 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-result = str(a * b * c)
+result = list(str(a * b * c))
 
 for i in range(10):
-    i = str(i)
-
-    print(result.count(i))
+    print(result.count(str(i)))
