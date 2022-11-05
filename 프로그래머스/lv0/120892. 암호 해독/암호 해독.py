f7daab965716951c15fcc2f1882a3d0c20322ae9
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2022-11-06 01:38:06 +0900

    [level 0] Title: 암호 해독, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,10 @@
+def solution(cipher, code):
+    answer = ''
+    #print(len(cipher))
+    for i in range(1,len(cipher)+1):
+        if i%code==0:
+            answer+=cipher[i-1]
+        
+        
+            
+    return answer
