6a386d3ad2bf7e69dde5a3b6807968e8593f584f
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-01-05 21:40:37 +0900

    Update 뒤에서 5등 위로.js

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,4 +1,8 @@
-function solution(num_list) {
+function solution1(num_list) {
+    return num_list.sort((a,b) => a-b).slice(5);
+}
+
+function solution2(num_list) {
     var answer = num_list.sort((a,b) => a-b);  // sort() 함수는 요소들을 문자열로 취급
     return answer.slice(5,answer[answer-1]);
 }
