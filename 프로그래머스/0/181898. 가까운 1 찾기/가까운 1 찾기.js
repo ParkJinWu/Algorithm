db50f27328ba1a1425bf603e20c4fc5b883a5ef5
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-01-20 19:08:48 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,10 @@
+function solution(arr, idx) {
+    
+    for(let i = 0 ; i<arr.length ; i++){
+        if(i >= idx && arr[i] === 1){
+            return i;
+        } 
+    }
+    return -1;
+    
+}
