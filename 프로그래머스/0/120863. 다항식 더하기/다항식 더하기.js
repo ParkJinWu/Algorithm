a8b6f81d23ee53029f9226febf896869fea9fb8c
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-03-06 20:26:16 +0900

    [level 0] Title: 다항식 더하기, Time: 2.44 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,36 @@
+function solution(polynomial) {
+    var answer = '';
+    let splitArr = polynomial.split(' + ');
+    let xCnt = 0;
+    let num = 0 ;
+    
+    for(let i = 0 ; i < splitArr.length ; i++){
+        let el = splitArr[i];
+        
+        if(el.includes('x')){
+            const idx = el.indexOf('x');
+            const n = el.slice(0,idx);
+            xCnt+= !n ? 1 : Number(n);
+     
+        }else{ // x 미포함
+            num+=Number(el);
+        } 
+    }
+    console.log("xCnt : " + xCnt);
+    console.log("num : " + num);
+    
+    
+    
+    // x항과 상수 둘 다 있는경우
+    if(xCnt && num){
+        if(xCnt < 2) xCnt = '';
+        return xCnt+'x + '+num;  
+    }else{
+        if(xCnt){ // x항만
+            if(xCnt < 2) xCnt = '';
+            return xCnt + 'x';
+        }else{ // 상수항만
+            return num +'';
+        }  
+    }
+}
