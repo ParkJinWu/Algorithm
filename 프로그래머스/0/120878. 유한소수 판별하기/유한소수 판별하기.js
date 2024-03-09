6b3ec48c6ed753595fef2c535ec14635e0aca857
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-03-09 15:08:01 +0900

    [level 0] Title: 유한소수 판별하기, Time: 2.35 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,23 +1,21 @@
 function solution(a, b) {
-    var arr = new Set();
+    let arr = [];
     let gcd = 1;
-    for(let i = 1 ; i <= Math.min(a,b) ; i++){
-        if(a % i == 0 && b % i == 0) gcd = i ;
+    for(let i = 1; i <= Math.min(a,b) ; i++){
+        if(a % i == 0 && b % i == 0) gcd = i;
     }
     
-   let bm = b / gcd ; // 분모(b)를 최대공약수로 나눠 기약분수의 분모로 만든다.
+    let bm = b / gcd;
     
-    console.log("분모 : " + bm);
-    for(let i = 2 ; i <= bm ; i++){
-        while(bm % i == 0 ){
-            console.log("@" + bm)
+    for(let i = 2; i <= bm ; i++){
+        while(bm % i == 0){
+            console.log(i);
+            arr.push(i);
             bm/=i;
-            arr.add(i);
-        } 
+        }
     }
     
-    const answer = [...arr].every((el) => (el == 2 || el == 5));
+    let answer = arr.every((el) => el == 2 || el == 5);
     
-    return answer ? 1 : 2;
-
+    return answer ? 1: 2;
 }
