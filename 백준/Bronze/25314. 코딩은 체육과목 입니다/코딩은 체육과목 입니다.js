f8c784a1791e75c2a20ec79d2a8488129f04e4a5
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-03-14 20:47:41 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,10 +1,4 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString();
+let n = Number(fs.readFileSync('/dev/stdin').toString());
 
-let n = Number(input/4);
-let str = '';
-
-for(let i = 0 ; i < n ; i++){
-        str+='long ';
-    }
-    console.log(str+'int');
+console.log(`${"long ".repeat(n/4) + "int"}`)
