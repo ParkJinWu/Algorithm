97e058f52dff79231d0dc756b15d8cbbec1014e0
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-03-27 21:20:46 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -3,14 +3,14 @@
 const [T,...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 for(let i = 0 ; i < T ; i++){
-    let arr = '';
+    let str = '';
 
     const [R,S] = input[i].split(' ');
 
     for(let j = 0 ; j < S.length ; j++){
-        arr+= S[j].repeat(R);
+        str+= S[j].repeat(R);
     }
     
-    console.log(arr);
+    console.log(str);
 }
 
