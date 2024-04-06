219f39db9459c9a7e73b9b0c25eea7bd09184483
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-04-06 19:54:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,11 +1,10 @@
-const word = require('fs').readFileSync('/dev/stdin').toString().trim()
+let word = require('fs').readFileSync('/dev/stdin').toString().trim()
 let croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
 
-function solution(word){
+
+
     for(let x of croatia){
         word = word.replaceAll(x,'@'); 
     }
-    return word.length;
-};
-
-console.log(solution(word));
+    
+    console.log(word.length);
