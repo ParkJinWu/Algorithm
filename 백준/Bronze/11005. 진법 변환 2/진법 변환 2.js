478e8f64de518495422a6a2506fb0e93cb7ffb0c
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-04-20 20:39:52 +0900

    [Bronze I] Title: 진법 변환 2, Time: 232 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.js
@@ -0,0 +1,30 @@
+//백준 : 진법 변환 2
+let [N,B] = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
+//let result = '';
+let result = [] ;// 문자열이 아닌 배열로 초기화한 이유는 나중에 reverse() 해야하기때문에
+
+while(N >= B){
+    let remain = N % B; // 나머지
+    let share = Math.trunc(N / B) ; // 몫
+
+    //console.log("몫 : " ,N);
+    //console.log("나머지 : ",remain);
+
+    if(remain >=10){
+        result.push(String.fromCharCode(remain+55));
+    }else{
+        result.push(remain);
+        
+    }
+
+    N = share;
+
+}
+
+if(N >=10){ // 마지막 나머지
+    result.push(String.fromCharCode(N+55));
+}else{
+    result.push(N);
+}
+
+console.log(result.reverse().join(''));
