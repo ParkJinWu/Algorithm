51d2cafff07706e59be4a660de795d3026817223
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-04-26 15:03:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    var answer = 0;
+    let cnt = 0;
+    while(num != 1){
+        if(num % 2 == 0){
+            num = num / 2
+        }else{
+            num = num * 3 + 1;
+        }
+        cnt++;
+    }
+    return cnt < 500 ? cnt : -1;
+}
