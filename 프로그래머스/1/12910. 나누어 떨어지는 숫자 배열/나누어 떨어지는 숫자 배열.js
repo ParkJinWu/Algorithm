91e054eeb659bf05b194b68c7b7658edfaa2d85e
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-04-26 14:50:00 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,7 @@
+function solution(arr, divisor) {
+    var answer =  arr.filter(el => {
+        return el % divisor === 0; 
+    });
+   
+    return answer.length == 0 ? [-1] : answer.sort((a,b) => a-b);
+}
