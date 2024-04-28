2ed11936f1b413a41ade5b446de81250433ee368
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-04-28 17:06:35 +0900

    [level 2] Title: 올바른 괄호, Time: 27.45 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    var answer = true;
+    let cnt = 0 ;
+    
+    if(s[0] === ')') return false;    
+    
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] === '(') cnt++;
+        else cnt--;
+        
+        if(cnt < 0) return false;
+
+    }
+
+    return cnt == 0;
+}
