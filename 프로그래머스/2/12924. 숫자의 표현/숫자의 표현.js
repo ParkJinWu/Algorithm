0b9d1ca796718eb5688003ccadb14415be37dcbe
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-01 21:28:31 +0900

    [level 2] Title: 숫자의 표현, Time: 1.67 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let boundary = n/2;
+    let cnt = 1 ;
+    
+    for(let i = 1 ; i < boundary ; i++){
+        let total = 0;
+        for(let j = i ; j<=n ; j++){
+            total += j; 
+            if(total === n) cnt++
+            if(total > n) break
+        }
+        
+    }
+    return cnt;
+}
