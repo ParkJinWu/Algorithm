c50c53cbbac79d9b1692eeae811179d5f6920b28
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-02 20:11:57 +0900

    [level 1] Title: 시저 암호, Time: 77.20 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,29 @@
+function solution(s, n) {
+    var answer = '';
+    s.split('').forEach((x,i) => {
+        if(x === ' ') answer += ' ';
+        else {
+            let unicode = x.charCodeAt();
+            let newCode = unicode + n;
+            console.log(newCode);
+            if(unicode >= 65 && unicode <= 90){
+                if(newCode > 90){
+                      answer += String.fromCharCode(newCode-26);
+                }else{
+                      answer += String.fromCharCode(newCode);
+                }
+            }
+            
+            if(unicode >= 97 && unicode <= 122){
+                if(newCode > 122){
+                      answer += String.fromCharCode(newCode-26);
+                }else{
+                      answer += String.fromCharCode(newCode);
+                }
+            }
+            
+        
+        }
+    })
+    return answer;
+}
