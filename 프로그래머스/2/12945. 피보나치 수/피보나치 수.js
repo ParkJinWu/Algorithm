0a0f128d9713a4de7f9677121fb76f129cb06482
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-02 22:31:46 +0900

    [level 2] Title: 피보나치 수, Time: 43.61 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    let fibo = [0,1];
+    
+    for(let i = 2 ; i <=n ; i++){
+        fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+    }
+    
+    return fibo[n];
+}
+
