b67bed1a811e93a09da2c492aedbf6ea45379b77
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-11 02:29:36 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int odd = 0, even = 0;
+        for(int i = 0; i < num_list.length ; i++){
+            if(i % 2 == 0){ //짝수
+                odd += num_list[i];
+            }else{
+                even += num_list[i];
+            }
+            
+        }
+        return Math.max(odd,even);
+    }
+}
