c73cc0115a86a28cadc577aedc58c75872481a81
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-14 16:50:09 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0 ; i < input.length(); i++){
+            char ch = input.charAt(i);
+            if(Character.isUpperCase(ch)){ //대문자일 때
+                sb.append(Character.toLowerCase(ch));
+            }else{
+                sb.append(Character.toUpperCase(ch));
+            }
+        }
+        System.out.print(sb);
+    }
+}
