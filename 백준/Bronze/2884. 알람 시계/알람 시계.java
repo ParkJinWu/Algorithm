c6f30d37f49cbc1e6cf6915b6016392bff76fc03
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-15 22:32:23 +0900

    [Bronze III] Title: 알람 시계, Time: 228 ms, Memory: 18484 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int MAX_HOUR = 24;
+        final int MAX_MIN = 60;
         Scanner sc = new Scanner(System.in);
         int hour = sc.nextInt();
         int min = sc.nextInt();
 
         if(min < 45){
             hour--;
-            min = 60 - (45-min);
+            min = MAX_MIN - (45-min);
 
-            if(hour<0) hour = 23;
+            if(hour<0) hour = MAX_HOUR+hour;
             System.out.println(hour + " " + min);
         }else {
             System.out.println(hour + " " +(min-45));
