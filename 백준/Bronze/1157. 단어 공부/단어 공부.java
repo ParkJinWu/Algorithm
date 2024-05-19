7a77feb4f66f33dc5dc3c805e5b08580fbd94f97
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-20 02:03:58 +0900

    [Bronze I] Title: 단어 공부, Time: 276 ms, Memory: 21132 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -2,25 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         String str = br.readLine().toUpperCase();
 
-        int cnt = 0;
+        int cnt = 0, max =0 , index =0;
 
         int alphabet[] = new int[26];
-        Arrays.fill(alphabet,0);
 
         for(int i = 0 ; i < str.length() ; i++){
             int idx = str.charAt(i) - 65;
             alphabet[idx]++;
         }
-        int index = 0;
-        int max = 0;
+
+
         for(int i = 0 ; i < alphabet.length ; i++){
             if(alphabet[i] > max){
                 max = alphabet[i];
