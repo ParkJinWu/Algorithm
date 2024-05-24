2014a56fd3337cdec088702e0622b393cede1e00
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-25 01:03:35 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(token.nextToken());
+        int B = Integer.parseInt(token.nextToken());
+
+        int lcm = (A / gcd(A,B)) * ( B / gcd(A,B)) * gcd(A,B);
+
+        System.out.println(gcd(A,B));
+        System.out.println(lcm);
+        
+    }
+
+    public static int gcd(int a,int b){
+        int large =  Math.max(a,b);
+        int small = Math.min(a,b);
+
+        if(small == 0) return large;
+
+        return gcd(small, large % small);
+    }
+}
