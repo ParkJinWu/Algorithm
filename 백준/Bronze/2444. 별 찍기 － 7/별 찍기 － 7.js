7c74e095aa9dffb9464bc07108f270c5731c77a8
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-27 19:12:55 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 9664 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -1,16 +1,11 @@
+// 백준 : 별찍기 - 7
 const fs = require('fs');
 const N = Number(fs.readFileSync('/dev/stdin'));
-
-/* 2. 1부터 N-1까지 공백과 별 찍기 */
-for(let i=1; i < N; i++){
-    let blank = ' '.repeat( (N-i) );
-    let stars = '*'.repeat( i+(i-1) );
-    console.log( blank + stars );
+for(let i = 1 ; i <= N; i++){
+    
+    console.log(' '.repeat(N-i) + '*'.repeat(i + (i-1) ));
+    
 }
-
-/* 3. N부터 1까지 공백과 별 찍기 */
-for(let j=N; j > 0; j--) {
-    let blank = ' '.repeat( (N-j) );
-    let stars = '*'.repeat( j+(j-1) );
-    console.log( blank + stars );
+for(let j = N-1 ; j > 0 ; j--){
+    console.log(' '.repeat(N-j) +'*'.repeat(j + (j-1)));
 }
