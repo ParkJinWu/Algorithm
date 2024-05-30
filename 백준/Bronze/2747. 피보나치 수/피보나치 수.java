bb949959d7390de5eac9d58abfee4b6e93958eaa
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-05-30 17:31:22 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        dp = new long[N+1];
+
+        System.out.println(dp_fibo(N));
+
+    }
+
+    public static long dp_fibo(int n){
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i =2 ; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
