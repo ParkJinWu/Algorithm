09bd8531e4caf6cde3e3aca8cce927fa34a88a93
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-06-01 18:31:27 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -9,21 +9,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
-        ArrayList<Integer> list = new ArrayList<Integer>();
+
         int A = Integer.parseInt(token.nextToken());
         int B = Integer.parseInt(token.nextToken());
+        int[] arr = new int[B];
+        int idx =0, sum = 0;
 
-        int sum = 0;
-
-        for(int i = 0 ; i < 1000 ; i++){
-            for(int j = 0 ; j <= i ; j++){
-                list.add(i+1);
+        for(int i = 1 ; i <= B ; i++){
+            for(int j = 1 ; j <=i ; j++){
+                if(idx == B) break;
+                arr[idx] = i;
+                idx++;
             }
         }
-
-        for(int i = A-1 ; i <= B-1 ; i++){
-            sum += list.get(i);
+        
+        for(int i = A - 1 ; i <= B-1 ;i++){
+            sum+= arr[i];
         }
+
         System.out.println(sum);
 
     }
