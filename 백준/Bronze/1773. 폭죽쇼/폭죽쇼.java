cdb1dcd738ea3e2df888f0e97acc26f5178e82df
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-06-24 18:49:52 +0900

    [Bronze II] Title: 폭죽쇼, Time: 376 ms, Memory: 22116 KB -BaekjoonHub

---
+++ 백준/Bronze/1773. 폭죽쇼/폭죽쇼.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(token.nextToken());
+        int C = Integer.parseInt(token.nextToken());
+
+        int[] arr = new int[C];
+
+        for(int i = 0 ; i<N ; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            int idx = 1;
+            int n ;
+            while (true){
+                n = num * idx++;
+                if(n >C) break;
+                arr[n-1]++;
+
+            }
+        }
+
+        long cnt = Arrays.stream(arr).filter(el -> el > 0).count(); //count()메서드의 반환 값은 long
+
+        System.out.print(cnt);
+
+
+
+    }
+}
