4d0b402612ed6b820f541de1b04aba00edc0f3fa
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-06-28 18:52:10 +0900

    Create 파일정리2.java

---
+++ 백준/Silver/20291. 파일 정리/파일정리2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    //파일 정리 - Stream 사용
+    //확장자별로 구분 & 확장자의 사전순으로 내림차순
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String,Integer> map = new HashMap<>();
+        StringTokenizer token;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < N ; i++){
+            String[] url = br.readLine().split("\\.");
+            String extension = url[1];
+            map.put(extension,map.getOrDefault(extension,0)+1);
+        }
+
+        map.entrySet()
+                .stream()
+                .sorted((e1,e2) -> e1.getKey().compareTo(e2.getKey()))
+                .forEach(el -> System.out.println(el.getKey() + " " + el.getValue()));
+    }
+}
