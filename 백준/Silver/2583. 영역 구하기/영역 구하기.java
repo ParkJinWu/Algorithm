1e7cd65441e0c305258ff53b2f62286537fb7c32
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-07-05 00:08:39 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
-//영역 구하기
 public class Main {
-    static int rows,cols,count;
+    static int rows, cols, count;
     static int[][] map;
+    static boolean[][] visited;
     static ArrayList<Integer> result = new ArrayList<>();
 
-    static int dx[] = {0,0,-1,1}; //x축 이동 (좌우)
-    static int dy[] = {-1,1,0,0}; //y축 이동 (상하)
+    //상하좌우 이동 방향
+    static int dx[] = {0, 0, -1, 1};
+    static int dy[] = {-1, 1, 0, 0};
 
-    public static void dfs(int x, int y){
-        map[x][y] = 1; // 현재위치 방문처리
-        count++; //영역의 개수 추가
+    public static void dfs(int x, int y) {
+        visited[x][y] = true; // 현재 위치 방문 처리
+        count++; // 영역 개수 증가
 
-        //상하좌우 이동
-        for(int i = 0 ; i < 4 ; i++){
+        for (int i = 0; i < 4; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
 
-            if(nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols){
-                if(map[nextX][nextY] == 0 ){
-                    dfs(nextX,nextY);
+            if (nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols) {
+                if (map[nextX][nextY] == 0 && !visited[nextX][nextY]) {
+                    dfs(nextX, nextY);
                 }
             }
-
         }
     }
 
@@ -34,49 +35,43 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
 
-        rows = Integer.parseInt(token.nextToken());
-        cols = Integer.parseInt(token.nextToken());
+        rows = Integer.parseInt(token.nextToken()); //가로 -> 세로(열)
+        cols = Integer.parseInt(token.nextToken()); //세로 -> 가로(행)
         int K = Integer.parseInt(token.nextToken());
 
+        //배열 초기화
         map = new int[rows][cols];
+        visited = new boolean[rows][cols]; // 방문 배열 초기화
 
-        for(int k = 0 ; k < K ; k++){
+        for (int k = 0; k < K; k++) {
             token = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(token.nextToken());
             int y1 = Integer.parseInt(token.nextToken());
             int x2 = Integer.parseInt(token.nextToken());
             int y2 = Integer.parseInt(token.nextToken());
 
-            for(int y = y1 ; y < y2 ;y++){
-                for(int x = x1; x < x2 ; x++){
+            for (int y = y1; y < y2; y++) {
+                for (int x = x1; x < x2; x++) {
                     map[y][x] = 1;
                 }
             }
+        }//for K
 
-        }
-
-//        for(int[] a : map){
-//            System.out.println(Arrays.toString(a));
-//        }
-
-        //전체 순회
-        for(int i = 0 ; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(map[i][j] == 0){
-                    count = 0 ;
-                    dfs(i,j); // 전체 순회
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    count = 0;
+                    dfs(i, j); // (0,0)시작
                     result.add(count);
                 }
             }
         }
 
         Collections.sort(result);
+
         System.out.println(result.size());
-        for(int n : result){
+        for (int n : result) {
             System.out.print(n + " ");
         }
-
-
-
     }
 }
