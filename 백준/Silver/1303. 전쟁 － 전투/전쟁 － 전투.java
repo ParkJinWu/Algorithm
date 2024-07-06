0562b55c795aca72d01af260768770fcf85eec4f
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-07-06 22:48:22 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 132 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //전쟁 - 전투
-//상하좌우 모여있으면 제곱
+//상하좌우 이동가능, N명이 뭉쳐있을 땐 N^2의 위력
 public class Main {
-    static int rows,cols,w_count,b_count,count;
+    static int rows,cols,count;
     static char[][] map;
     static boolean[][] visited;
+    static int white_count, black_count;
 
+    //상하좌우 이동 방향
     static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
 
     public static void dfs(int x, int y, char color){
-        visited[x][y] = true;
+        visited[x][y] = true; //현재 위치 방문 처리
         count++;
 
         for(int i = 0 ; i < 4 ; i++){
             int nextX = x + dx[i];
             int nextY = y + dy[i];
 
-            if(nextX >= 0 && nextX < cols && nextY >= 0 && nextY <rows){
+            if(nextX >= 0 && nextX < cols && nextY >= 0 && nextY < rows){
                 if(!visited[nextX][nextY] && map[nextX][nextY] == color){
                     dfs(nextX,nextY,color);
                 }
             }
         }
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,35 +40,33 @@ public class Main {
         rows = Integer.parseInt(token.nextToken()); //가로
         cols = Integer.parseInt(token.nextToken()); //세로
 
-        //배열 크기 초기화
         visited = new boolean[cols][rows];
         map = new char[cols][rows];
 
-        //지도에 정보 입력
-        for(int i = 0; i < cols ; i++){ //세로
+        for(int i = 0 ; i < cols ; i++){
             String line = br.readLine();
-            for(int j = 0; j < rows ; j++){ //가로
-                map[i][j] = line.charAt(j) ;
+            for(int j = 0 ; j < rows; j++){
+                map[i][j] = line.charAt(j);
             }
         }
 
-        //전체 지도 탐색
-        for(int i = 0 ; i < cols ; i++){ //세로
-            for(int j = 0 ; j < rows ; j++){ //가로
+        white_count = 0;
+        black_count = 0;
+
+        for(int i = 0 ; i < cols ; i++){
+            for(int j = 0 ; j < rows; j++){
                 if(!visited[i][j]){
                     count = 0;
-                    char color = map[i][j];
-                    dfs(i,j,color);
-                    if(color == 'W'){
-                        w_count += count * count;
+                    dfs(i,j,map[i][j]); //(0,0)시작
+                    if(map[i][j] == 'W'){
+                        white_count += Math.pow(count,2);
                     }else {
-                        b_count += count * count;
+                        black_count += Math.pow(count,2);
                     }
-
                 }
             }
         }
-        System.out.println(w_count + " " + b_count);
 
+        System.out.println(white_count + " " + black_count);
     }
 }
