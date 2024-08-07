da49c0e8a150d614b639252eb98dfbc6e5a7d775
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-07 10:55:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.05 ms
+메모리: 91.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 08일 00:01:57
+2024년 08월 07일 10:55:56
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,10 +1,22 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for(int i = 0 ; i < signs.length ; i++){
-            if(signs[i]) answer+=absolutes[i];
-            else answer -= absolutes[i];
+        for (int i = 0; i < absolutes.length; i++) {
+            if(signs[i]){
+                answer += absolutes[i];
+            }else{
+                answer -= absolutes[i];
+            }
         }
         return answer;
     }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true,false,true};
+
+        System.out.println(sol.solution(absolutes,signs));
+    }
 }
+
