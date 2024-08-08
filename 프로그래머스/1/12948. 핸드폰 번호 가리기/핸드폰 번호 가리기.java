3a791e512344977d0c81944230d106a93d9d3e05
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-08 10:20:33 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.94 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,11 +1,9 @@
-class Solution{
+class Solution {
     public String solution(String phone_number) {
-        String answer = "";
-        String privacy = phone_number.substring(0,phone_number.length()-4);
-
-        answer =  "*".repeat(privacy.length()) + phone_number.substring(phone_number.length()-4);
-
-        return answer;
+        int length = phone_number.length();
+        String maskedPart = "*".repeat(length - 4);
+        String visiblePart = phone_number.substring(length - 4);
+        return maskedPart + visiblePart;
     }
 
     public static void main(String[] args) {
