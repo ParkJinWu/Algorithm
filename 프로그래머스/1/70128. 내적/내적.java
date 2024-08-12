961ef931d48b3b947070b8bfa7e4c9ca83ab621a
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-12 10:33:00 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,16 @@
-class Solution {
+public class Solution {
     public int solution(int[] a, int[] b) {
-        int sum = 0;
-        for(int i = 0 ; i < a.length ; i++){
-            sum += a[i] * b[i];
+        int answer = 0;
+        for (int i = 0; i <a.length ; i++) {
+            answer += a[i] * b[i];
         }
-        return sum;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        //int[] a = {1,2,3,4}; int[] b= {-3,-1,0,2};
+        int[] a = {-1,0,1}; int[] b= {1,0,-1};
+        System.out.println(sol.solution(a,b));
     }
 }
