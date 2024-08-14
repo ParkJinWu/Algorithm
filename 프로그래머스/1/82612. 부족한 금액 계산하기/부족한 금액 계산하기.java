049012fe8dda168feba67b38114315de797481f4
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-14 10:26:42 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.17 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,12 +1,17 @@
-class Solution {
+public class Solution {
     public long solution(int price, int money, int count) {
-        long total = 0;
-    
-        
-        for(int i = 1 ; i <= count ; i++){
-            total += i * price;
+        long sum = 0;
+        for(int i = 1; i <= count ; i++){
+            sum += price * i;
         }
-        long answer = total - money ;
-        return  answer > 0 ? answer : 0;
+        return sum < money ? 0 : sum - money;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int price = 3;
+        int money = 20;
+        int count = 4;
+        System.out.println(sol.solution(price,money,count));
     }
 }
