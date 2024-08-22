b1c372f4918b5ce98c503c245cd76e9602b3f053
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-22 18:34:27 +0900

    [level 1] Title: 최소직사각형, Time: 1.51 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 34.66 ms
+메모리: 88.5 MB, 시간: 1.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 16:28:19
+2024년 08월 22일 18:34:24
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,23 +1,17 @@
-import java.util.*;
-
-class Solution {
+public class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int min = 0;
-        int max = 0;
-        
-        for(int i = 0 ; i < sizes.length ; i++){
-            if(min < Math.min(sizes[i][0],sizes[i][1])){
-                min = Math.min(sizes[i][0],sizes[i][1]);
-            }
-            if(max < Math.max(sizes[i][0],sizes[i][1])){
-                max = Math.max(sizes[i][0],sizes[i][1]);
-            }
-            
-            System.out.println();
-            
+        int max = 0; int min = 0;
+        for(int i = 0 ; i < sizes.length; i++){
+            max = Math.max(max,Math.max(sizes[i][0],sizes[i][1]));
+            min = Math.max(min,Math.min(sizes[i][0],sizes[i][1]));
         }
-        
-        return max*min;
+        return max * min;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] sizes = {{60,50},{30,70},{60,30},{80,40}};
+        System.out.println(sol.solution(sizes));
     }
 }
