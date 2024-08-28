068166af6dde0f6fb27508d0389b69252aaae47f
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-08-28 20:46:54 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,36 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-class Solution {
+public class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        for(int i = 0 ; i < commands.length ; i++){
-                int a = commands[i][0];
-                int b = commands[i][1];
-                int k = commands[i][2];
-                
-                //a-1 ~ b-1
-                int[] arr = Arrays.copyOfRange(array,a-1,b);
-                Arrays.sort(arr);
-                answer[i] = arr[k-1];
+
+        for (int i = 0; i < commands.length; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int idx = commands[i][2];
+
+            // Sublist를 생성
+            for (int j = start; j <= end; j++) {
+                list.add(array[j - 1]);
+            }
+
+            // 정렬
+            Collections.sort(list);
+
+            // 정렬된 리스트에서 idx번째 수를 가져오기
+            answer[i] = list.get(idx - 1);
         }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(sol.solution(array, commands)));
     }
 }
