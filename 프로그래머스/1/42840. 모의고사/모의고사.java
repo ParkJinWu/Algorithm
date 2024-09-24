953454786050e60124c32c2f1d0fa19647b9c9a3
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-09-24 18:54:47 +0900

    [level 1] Title: 모의고사, Time: 14.55 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,44 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-class Solution {
+public class Solution {
     public int[] solution(int[] answers) {
+        int[] score = new int[3];
+        int[] supo1 = {1,2,3,4,5};
+        int[] supo2 = {2,1,2,3,2,4,2,5};
+        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
 
-        int[] scores = new int[3];        
-        int[] num1 = {1,2,3,4,5};
-        int[] num2 = {2,1,2,3,2,4,2,5};
-        int[] num3 = {3,3,1,1,2,2,4,4,5,5};
-        
-        for(int i = 0 ; i < answers.length ; i++){
-            if(answers[i] == num1[i % num1.length]){
-                scores[0]++;
-            }if(answers[i] == num2[i % num2.length]){
-                scores[1]++;
-            }if(answers[i] == num3[i % num3.length]){
-                scores[2]++;
+        for(int i = 0; i < answers.length ; i++){
+            if(answers[i] == supo1[i % supo1.length]){
+                score[0]++;
+            }
+            if(answers[i] == supo2[i % supo2.length]){
+                score[1]++;
+            }
+            if(answers[i] == supo3[i % supo3.length]){
+                score[2]++;
             }
         }
-        
 
-        List<Integer> list = new ArrayList<>();
-        int max = Math.max(scores[0], Math.max(scores[1],scores[2]));
-        
-        
-        for(int i = 0 ; i < scores.length ; i++){
-            if(max == scores[i]){
-                list.add(i+1);
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int maxScore = Arrays.stream(score).max().getAsInt();
+
+        for (int i = 0; i < score.length; i++) {
+            if(score[i] == maxScore){
+                result.add(i+1);
             }
         }
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+
+        // int -> array
+        return result.stream().mapToInt(i -> i).toArray();
     }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] answers = {1,2,3,4,5};
+        //int[] answers = {1,3,2,4,2};
+        System.out.println(Arrays.toString(sol.solution(answers)));
+    }
+
 }
