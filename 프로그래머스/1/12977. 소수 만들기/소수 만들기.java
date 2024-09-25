b5dd819086bef1c57b8b4e607539436becae51c6
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-09-25 18:50:41 +0900

    [level 1] Title: 소수 만들기, Time: 10.45 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,35 +1,36 @@
-import java.util.*;
-
-class Solution {
+public class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        Arrays.sort(nums);
-
-        System.out.println(Arrays.toString(nums));
-        for(int i = 0 ; i < nums.length -2 ; i++){
-            for(int j = i+1 ; j < nums.length - 1; j++){
-                for(int k = j + 1 ; k < nums.length; k++){
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1 ; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
                     int sum = nums[i] + nums[j] + nums[k];
-                    if(isPrime(sum)){
-                        //System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
-                        answer++;
-                    }
+                    if(isPrime(sum)) answer++;
                 }
             }
+            
         }
 
         return answer;
     }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        //int[] nums = {1,2,3,4};
+        int[] nums = {1,2,7,6,4};
+        System.out.println(sol.solution(nums));
+    }
+
     public static boolean isPrime(int n){
         if(n <= 1) return false;
         if(n == 2) return true;
-        
+
+        // 짝수
         if(n % 2 == 0) return false;
-        
-        for(int i = 3; i < n ; i+=2){
+
+        for (int i = 3; i < n ; i+=2) {
             if(n % i == 0) return false;
         }
-        
         return true;
     }
 }
