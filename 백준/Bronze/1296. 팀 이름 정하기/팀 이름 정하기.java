6e77eea3e311c92a65bee0a32c889ae55cf4bc5e
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-11-16 16:43:55 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.java
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.java
@@ -1,49 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String myTeam = br.readLine();
+        String yeondu = br.readLine();
         int N = Integer.parseInt(br.readLine());
 
-        int bestScore = -1;    // 가장 높은 점수
         String bestTeam = "";
-        for (int i = 0; i < N; i++) {
-            String otherTeam = br.readLine();
-            int score = getFormula(myTeam,otherTeam);
+        int bestScore = Integer.MIN_VALUE;
 
+        for (int i = 0; i < N; i++) {
+            String otherTeam= br.readLine();
+            int score = getScore(yeondu,otherTeam);
             if(score > bestScore || score == bestScore && otherTeam.compareTo(bestTeam) < 0){
-                bestScore = score;
                 bestTeam = otherTeam;
+                bestScore = score;
             }
         }
         System.out.println(bestTeam);
-
     }
 
-    private static int getFormula(String myTeam , String team){
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+    private static int getScore(String myTeam, String otherTeam){
+        HashMap<Character,Integer> map = getMap(myTeam,otherTeam);
 
-        for (char c : myTeam.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : team.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-
-        //((L+O) × (L+V) × (L+E) × (O+V) × (O+E) × (V+E)) mod 100
         // LOVE 각 문자의 개수 계산
         int L = map.getOrDefault('L', 0);
         int O = map.getOrDefault('O', 0);
         int V = map.getOrDefault('V', 0);
         int E = map.getOrDefault('E', 0);
 
-        // 공식 계산
+        // 공식 계산: ((L+O) × (L+V) × (L+E) × (O+V) × (O+E) × (V+E)) mod 100
         return ((L+O) * (L+V) * (L+E) * (O+V) * (O+E) * (V+E)) % 100;
+    }
+
+    private static HashMap<Character,Integer> getMap(String myTeam, String otherTeam){
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        // 연두 팀의 문자 개수 계산
+        for(char c : myTeam.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        // 다른 팀의 문자 개수 추가
+        for(char c : otherTeam.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        return map;
     }
 }
