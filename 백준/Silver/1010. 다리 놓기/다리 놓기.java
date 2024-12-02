f3756bd68f28606d478817b2ecb0c16370f03335
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2024-12-02 17:37:22 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
+    static long[][] dp = new long[30][30] ;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer token;
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
-        for (int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 서쪽 사이트 개수
-            int M = Integer.parseInt(st.nextToken()); // 동쪽 사이트 개수
+        for (int i = 0; i < T; i++) {
+            token = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(token.nextToken());
+            int M = Integer.parseInt(token.nextToken());
 
-            // 팩토리얼 기반 이항 계수 계산
-            BigInteger facM = factorial(M);
-            BigInteger facN = factorial(N);
-
-            // C(M, N) = M! / (N! * (M-N)!)
-            BigInteger result = facM.divide(facN.multiply(factorial(M - N)));
-            sb.append(result).append("\n");
+            long c = getConbine(M,N);
+            sb.append(c).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    // BigInteger를 이용한 팩토리얼 계산
-    public static BigInteger factorial(int n) {
-        BigInteger fac = BigInteger.ONE;
-
-        for (int i = 2; i <= n; i++) {
-            fac = fac.multiply(BigInteger.valueOf(i));
+    public static long getConbine(int n, int m){
+        // 초기값 세팅
+        for (int i = 0; i < 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
         }
 
-        return fac;
+        for (int N = 1; N < 30 ; N++){
+            for (int M = 1; M < N; M++) {
+                dp[N][M] = dp[N -1][M -1] + dp[N -1][M];
+            }
+        }
+        return dp[n][m];
     }
 }
