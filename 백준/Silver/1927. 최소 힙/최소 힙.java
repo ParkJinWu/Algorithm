981a29e469855b7a5ddbb7ea17c9e72ddae270dc
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2025-01-03 17:34:57 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 26440 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -4,25 +4,28 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-// 최소 힙
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder(); // 출력 최적화
 
+        int N = Integer.parseInt(br.readLine());
         Queue<Integer> pq = new PriorityQueue<>();
+
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
 
-            if(num != 0){ // 0이 아니라면 큐에 넣는다.
-                pq.offer(num);
-            }else { // 0이라면 최솟값 출력 | 큐가 비어있다면 0 출력
-                if(pq.isEmpty()){
-                    System.out.println(0);
-                }else {
-                    System.out.println(pq.poll());
+            if (num != 0) {
+                pq.offer(num); // 0이 아니라면 큐에 추가
+            } else {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n"); // 큐가 비어 있으면 0 출력
+                } else {
+                    sb.append(pq.poll()).append("\n"); // 최솟값 출력
                 }
             }
         }
+
+        System.out.print(sb); // 최종 결과 한 번에 출력
     }
 }
