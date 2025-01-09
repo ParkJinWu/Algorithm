6b1325588998c3807b0573c2736cf0e37e2bca5f
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2025-01-09 16:11:15 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -6,97 +6,80 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 // 트리 순회
+// 루트는 항상 A
 public class Main {
+
     static class Node{
-        char node;
+        char value;
         Node left;
         Node right;
 
-        Node(char node, Node left, Node right){
-            this.node = node;
-            this.left = left;
-            this.right = right;
+        Node(char value){
+            this.value = value;
         }
     }
 
-    // 전위 순회 -> (루트) (왼쪽 자식) (오른쪽 자식)
+    // 전위 순회 root -> left -> right
     public static void preOrder(Node node){
         if(node != null){
-            System.out.print(node.node);
+            System.out.print(node.value);
             preOrder(node.left);
             preOrder(node.right);
         }
-    }
 
-    // 중위 순회 -> (왼쪽 자식) (루트) (오른쪽 자식)
+    }
+    // 중위 순회 left -> root -> right
     public static void inOrder(Node node){
         if(node != null){
             inOrder(node.left);
-            System.out.print(node.node);
+            System.out.print(node.value);
             inOrder(node.right);
         }
     }
-
-    // 후위 순회 -> (왼쪽 자식) (오른쪽 자식) (루트)
+    // 후위 순회 left -> right -> root
     public static void postOrder(Node node){
         if(node != null){
             postOrder(node.left);
             postOrder(node.right);
-            System.out.print(node.node);
+            System.out.print(node.value);
         }
     }
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
+
         Map<Character,Node> tree = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             StringTokenizer token = new StringTokenizer(br.readLine());
 
-            // 루트
+            // 부모
             char parents = token.nextToken().charAt(0);
-
-            // 왼쪽 자식
+            // 왼쪽 자식 노드
             char left = token.nextToken().charAt(0);
-
-            // 오른쪽 자식
+            // 오른쪽 자식 노드
             char right = token.nextToken().charAt(0);
 
-            if(!tree.containsKey(parents)){
-                tree.put(parents,new Node(parents,null,null));
+            // 노드 생성 및 연결
+            tree.putIfAbsent(parents,new Node(parents));
+
+            if(left != '.'){
+                tree.putIfAbsent(left, new Node(left));
+                tree.get(parents).left = tree.get(left);
             }
 
-            // 부모 노드가 없으면 생성
-            if(!tree.containsKey(parents)){
-                tree.put(parents, new Node(parents, null, null));
+            if(right != '.'){
+                tree.putIfAbsent(right,new Node(right));
+                tree.get(parents).right = tree.get(right);
             }
-
-            // 왼쪽 자식 노드가 없으면 생성
-            if(left != '.' && !tree.containsKey(left)){
-                tree.put(left, new Node(left, null, null));
-            }
-
-            // 오른쪽 자식 노드가 없으면 생성
-            if(right != '.' && !tree.containsKey(right)){
-                tree.put(right, new Node(right, null, null));
-            }
-
-            // 노드 연결
-            Node parentNode = tree.get(parents);
-            parentNode.left = left == '.' ? null : tree.get(left);
-            parentNode.right = right == '.' ? null : tree.get(right);
-
         }
 
-        Node root = tree.get('A'); // 항상 A가 루트
-
-        preOrder(root); // 전위
+        Node root = tree.get('A'); // 루트는 항상 A
+        preOrder(root);
         System.out.println();
-        inOrder(root); // 중위
+        inOrder(root);
         System.out.println();
-        postOrder(root); // 후위
+        postOrder(root);
     }
 }
