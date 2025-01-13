078b1e14cd14a8e9af5f42d66dbd842b391957bc
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2025-01-13 18:14:15 +0900

    [Silver I] Title: 스타트와 링크, Time: 400 ms, Memory: 18368 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,70 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 스타트와 링크
-// N 은 짝수
-// 두팀의 능력치 차이 출력 (최솟값)
 public class Main {
-    static boolean[] visited;
     static int N ;
+    static boolean[] visited;
     static int[][] map;
-    static int min;
+    static int minDiffValue;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        StringTokenizer token ;
 
-        map = new int[N+1][N+1];
-        visited = new boolean[N+1];
-        min = Integer.MAX_VALUE;
-
-        for (int i = 1; i <= N; i++) {
-            token = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(token.nextToken());
-            }
-        }
-
-        divideTeams(1, 0); // 1부터 시작
-
-        System.out.println(min);
-    }
-
-    private static void divideTeams(int idx, int count) {
-
+    // 팀을 나누고, 두 팀의 점수 차를 구하는 diffScore 함수 호출
+    private static void divideTeams(int order, int count) {
         if(count == N / 2){
             diffScore();
             return;
         }
 
-        for (int i = idx; i <= N ; i++) {
-            if(!visited[i]){// 방문하지 않았다면
-                visited[i] = true;// 방문 처리
-                divideTeams(i + 1,count + 1 );
+        for (int i = order; i < N ; i++) {
+            if (!visited[i]) { // 방문하지 않았다면
+                visited[i] = true; // 방문 처리
+                divideTeams(i + 1, count + 1);
                 visited[i] = false;
             }
-
         }
     }
 
+    // 두 팀의 점수 차이 구하는 함수
     private static void diffScore() {
+        // Start 팀과 Link 팀의 점수를 저장할 변수 초기화
         int teamStart = 0; int teamLink = 0;
 
-        for (int i = 1; i <= N ; i++) {
-            for (int j = i+1; j <= N; j++) {
-                if(visited[i] && visited[j]){ // 스타트
+        for (int i = 0; i < N ; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if(visited[i] && visited[j]){ // team start
                     teamStart += map[i][j] + map[j][i];
-                }else if(!visited[i] && !visited[j]) { // 링크
+                }else if(!visited[i] && !visited[j]){
                     teamLink += map[i][j] + map[j][i];
                 }
             }
         }
 
-        int diff = Math.abs(teamLink - teamStart);
-        min = Math.min(min, diff);
+        int diff = Math.abs(teamStart - teamLink);
+        minDiffValue = Math.min(diff,minDiffValue);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N];
+        minDiffValue = Integer.MAX_VALUE; // 가장 큰 값으로 초기화 (0 방지)
+
+        for (int i = 0; i < N; i++) {
+            token = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(token.nextToken());
+            }
+        }
+
+        // 팀 나누기
+        divideTeams(0,0);
+        System.out.println(minDiffValue);
     }
 }
