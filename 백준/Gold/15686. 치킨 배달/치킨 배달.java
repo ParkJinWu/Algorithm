b71f76131eba5445f7ebec4ee66a385044e04a15
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2025-01-20 18:39:45 +0900

    [Gold V] Title: 치킨 배달, Time: 240 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -12,92 +11,90 @@ import java.util.StringTokenizer;
 // |r1-r2| + |c1-c2|
 public class Main {
     static final int HOUSE = 1;
-    static final int CHICKEN_HOUSE = 2;
-    static int minResult = Integer.MAX_VALUE;
+    static final int CHICKEN = 2;
+    static List<int[]> chickenHouses;
+    static List<int[]> houses;
+    static int N , M;
     static boolean[] visited;
+    static int result = Integer.MAX_VALUE;
 
-    // 치킨 거리의 최솟값 계산 함수
-    public static int calMinChickenDistance(List<int[]> houses, List<int[]> chickens, int M){
+    public static int calMinChickenDistance(){
+        int totalDistance = 0;
 
-        if(M < chickens.size()){
-            visited = new boolean[chickens.size()];
-            combinationClosed(houses,chickens,0,M,0);
-            return minResult;
-
-        }else { // 폐업할 치킨집이 없는 경우
-            int totalDistance = 0;
-
+        if(chickenHouses.size() > M){
+            visited = new boolean[chickenHouses.size()];
+            combi(0,0);
+            return result;
+        }else {
             for (int[] house : houses){
-                int minDistance = Integer.MAX_VALUE;
-                for(int[] chicken : chickens){
+                int min = Integer.MAX_VALUE;
+                for (int[] chicken : chickenHouses){
                     int distance = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-                    minDistance = Math.min(minDistance, distance); // 최솟값 업데이트
+                    min = Math.min(min, distance);
                 }
-                totalDistance += minDistance;
+                totalDistance += min;
             }
-            return totalDistance;
         }
+        return totalDistance;
     }
 
-    // 폐업할 치킨집을 고르는 조합 함수
-    public static int combinationClosed(List<int[]> houses, List<int[]> chickens, int start, int M , int count){
+    public static void combi(int order, int count){
 
         if(count == M){
             int totalDistance = 0;
-            for(int[] house : houses){
-                int minDistance = Integer.MAX_VALUE;
-                for (int i = 0; i < chickens.size(); i++) {
-                    if(visited[i]){
-                        int[] chicken = chickens.get(i);
+            for (int[] house : houses){
+                int min = Integer.MAX_VALUE;
+
+                for (int i = 0; i < chickenHouses.size(); i++) {
+                    if(visited[i]){ // 조합으로 선택된 치킨집
+                        int[] chicken = chickenHouses.get(i);
                         int distance = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
-                        minDistance = Math.min(minDistance,distance);
+                        min = Math.min(min,distance);
                     }
                 }
-                totalDistance += minDistance;
+                totalDistance += min;
             }
-            minResult = Math.min(minResult, totalDistance);
+            result = Math.min(result, totalDistance);
+            return;
         }
 
-        // 백트래킹
-        for (int i = start; i < chickens.size() ; i++) {
+
+        for (int i = order; i < chickenHouses.size() ; i++) {
             if(!visited[i]){
                 visited[i] = true;
-                combinationClosed(houses,chickens,i + 1, M, count + 1);
+                combi(i + 1, count + 1);
                 visited[i] = false;
             }
         }
-        return minResult;
-    }
 
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(token.nextToken()); // 도시의 크기 N x N
-        int M = Integer.parseInt(token.nextToken()); // 폐업시키지 않을 치킨집의 수
+        N = Integer.parseInt(token.nextToken()); // 도시 크기 N x N
+        M = Integer.parseInt(token.nextToken()); // 폐업시키지 않을 치킨집의 개수
+        houses = new ArrayList<>(); // 집 좌표 저장 리스트
+        chickenHouses = new ArrayList<>(); // 치킨집 좌표 저장 리스트
 
+        int[][] city = new int[N+1][N+1];
 
-        int[][] city = new int[N+1][N+1]; // 1-index
-        List<int[]> chickenHouses = new ArrayList<>(); // 치킨집 좌표 저장 리스트
-        List<int[]> houses = new ArrayList<>(); // 치킨집 좌표 저장 리스트
-
-        // 치킨 도시 채우기
-        for (int i = 1; i <= N ; i++) {
+        for (int i = 1; i <= N  ; i++) {
             token = new StringTokenizer(br.readLine());
 
-            for (int j = 1; j <= N; j++) {
+            for (int j = 1; j <= N ; j++) {
                 city[i][j] = Integer.parseInt(token.nextToken());
 
                 if(city[i][j] == HOUSE){
                     houses.add(new int[]{i,j});
-                }
-                else if(city[i][j] == CHICKEN_HOUSE){
+                } else if (city[i][j] == CHICKEN) {
                     chickenHouses.add(new int[]{i,j});
                 }
             }
         }
 
-        System.out.println(calMinChickenDistance(houses,chickenHouses,M));
+        System.out.println(calMinChickenDistance());
+
     }
 }
