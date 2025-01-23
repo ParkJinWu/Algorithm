151e9fbcdd07be931d926f7fb04f80c37765f895
Park Jin Woo <110481735+ParkJinWu@users.noreply.github.com>
2025-01-23 22:11:31 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1032 ms, Memory: 74728 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,19 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 // 트리의 부모 찾기
 public class Main {
-    static ArrayList<Integer> connection[];
-    static boolean[] visited;
-    static StringTokenizer token;
     static int N;
-    static int[] parents;
+    static List<Integer> list[] ; // 연결 정보 리스트
+    static boolean[] visited; // 방문 여부 체크 배열
+    static int[] parents; // 부모 노드 저장 배열
+    static StringTokenizer token;
 
-    private static void  dfs(int index){
-        visited[index] = true; // 현재 노드 방문 처리
-        for(int next : connection[index]){
+    private static void dfs(int index){
+        visited[index] = true; // 방문 처리
+
+        for (int next : list[index]){
             if(!visited[next]){
                 parents[next] = index;
                 dfs(next);
@@ -25,31 +28,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        
+        // 변수 초기화
+        list = new ArrayList[N+1];
         visited = new boolean[N+1];
-        connection = new ArrayList[N+1];
         parents = new int[N+1];
 
-        // 연결 정보를 저장할 리스트 생성
-        for (int i = 0; i < N+1 ; i++) {
-            connection[i] = new ArrayList<>();
+        // 연결 정보를 저장할 빈 배열 생성
+        for (int i = 1; i <= N ; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        // 연결 정보 저장
-        for (int i = 1; i < N  ; i++) {
+        // 노드 양방향 연결
+        for (int i = 1; i < N ; i++) {
             token = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(token.nextToken());
             int B = Integer.parseInt(token.nextToken());
 
-            // 양방향 연결
-            connection[A].add(B);
-            connection[B].add(A);
+            list[A].add(B);
+            list[B].add(A);
         }
 
         dfs(1);
 
-        for (int i = 2; i <=N ; i++) {
+        for (int i = 2; i <= N ; i++) {
             System.out.println(parents[i]);
-
         }
+
     }
 }
